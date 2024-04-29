65ae2f5ab1a08078e7c05852b566368c2d06aa80
Lyle Simmons <lylesimmons125@gmail.com>
2024-04-28 20:59:44 -0400

    lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,22 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (1, 'Mr. Chocolate', 6, 'M', 'Brown', 'Mild', 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (2, 'Rowdy', 8, 'M', 'Black', 'Mild', 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (3, 'Tabitha', 12, 'F', 'Brown', 'High', 0);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (4, 'Sergeant Brown', 7, 'M', 'Brown', 'Mild', 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (5, 'Melissa', 6, 'F', 'Black', 'Mild', 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (6, 'Grinch', 9, 'M', 'Black', 'Low', 0);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (7, 'Wendy', 11, 'F', 'Brown', 'High', 1);
+
+INSERT INTO bears DEFAULT VALUES;
